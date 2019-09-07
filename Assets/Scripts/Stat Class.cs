cabab1580e8bde2e11bac9397785591e458f5873
elricv <elricv@users.noreply.github.com>
2019-09-07 13:30:03 +0800

    Crafting
    Added Crafting Script

---
+++ Assets/Scripts/Stat Class.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatClass
+{
+    public string NAME;
+    public int HP;
+    public int PWR;
+    public string STATUS;
+    public int SPD;
+	public int PRICE;
+
+    public StatClass(string NAME, int HP, int PWR, string STATUS, int SPD, int PRICE)
+    {
+        this.NAME = NAME;
+        this.HP = HP;
+        this.PWR = PWR;
+        this.STATUS = STATUS;
+        this.SPD = SPD;
+		this.PRICE = PRICE;
+    }
+
+
+}
