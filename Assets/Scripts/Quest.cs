8ed7dafdf91d8a18b210b3cd98d3fd09d6de8af2
GregMendoza <mendoza.francisgreg@gmail.com>
2019-09-14 12:27:18 +0800

    Quest Scripts
    Quest Scripts
    Added:
    Quest Class
    Quest Manager

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Quest {
+
+    public enum QuestProgress {NOT_AVAILABLE, AVAILABLE, ACCEPTED, COMPLETE, DONE}
+
+
+    #region QuestDetails
+
+
+    public int ID;
+    public string questName;
+    public string questDesc;
+    public int backToMenu;
+    public QuestProgress progress;
+
+    #endregion
+
+    #region Objectives
+
+
+    public string questObjective;
+    public int questObjectiveCount;
+    public int questRequirement;
+
+
+    #endregion
+
+    #region Rewards
+
+    public int fameReward;
+    public int goldReward;
+    public string itemReward;
+
+    #endregion
+
+
+}
---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour {
+
+    public static QuestManager questManager;
+    public List<Quest> questList = new List<Quest>();
+    public List<Quest> currentQuestList = new List<Quest>();
+ 
+
+    void Awake()
+    {
+        if(questManager = null)
+        {
+            questManager = this;
+        }
+
+        else if(questManager != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    //Accept Quest
+   public void AcceptQuest(int questID)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+
+             if (questList[i].ID == questID && questList[i].progress == Quest.QuestProgress.AVAILABLE)
+             {
+                currentQuestList.Add(questList[i]);
+                questList[i].progress = Quest.QuestProgress.ACCEPTED;
+
+             }
+            
+        }
+    }
+
+    //Abandon Quest
+    public void AbandonQuest(int questID)
+    {
+        for (int i = 0; i < currentQuestList.Count; i++)
+        {
+
+            if (currentQuestList[i].ID == questID && currentQuestList[i].progress == Quest.QuestProgress.ACCEPTED)
+            {
+                currentQuestList[i].progress = Quest.QuestProgress.AVAILABLE;
+                currentQuestList[i].questObjectiveCount = 0;
+                currentQuestList.Remove(currentQuestList[i]);
+
+
+            }
+
+        }
+    }
+
+
+
+    //Complete Quest
+    public void CompleteQuest(int questID)
+    {
+        for (int i = 0; i < currentQuestList.Count; i++)
+        {
+            if (currentQuestList[i].ID == questID && currentQuestList[i].progress == Quest.QuestProgress.COMPLETE)
+            {
+                currentQuestList[i].progress = Quest.QuestProgress.DONE;
+                currentQuestList.Remove(currentQuestList[i]);
+            }
+
+        }
+    }
+
+    #region QuestChecking
+    public bool RequestAvailabeQuest(int questID)
+    {
+        for(int i = 0; i < questList.Count; i++)
+        {
+            if (questList[i].ID == questID && questList[i].progress == Quest.QuestProgress.AVAILABLE)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public bool RequestAcceptedQuest(int questID)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            if (questList[i].ID == questID && questList[i].progress == Quest.QuestProgress.ACCEPTED)
+            {
+                return true;
+            }
+        }
+        return false; 
+    }
+
+
+    public bool RequestCompleteQuest(int questID)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            if (questList[i].ID == questID && questList[i].progress == Quest.QuestProgress.COMPLETE)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    #endregion
+
+
+
+}
