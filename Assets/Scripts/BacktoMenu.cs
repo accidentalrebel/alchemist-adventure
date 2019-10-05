ffb980801cb1c9ea4cd123e5561c935a55b857a9
GregMendoza <mendoza.francisgreg@gmail.com>
2019-10-05 13:37:03 +0800

    PrepPhase still in the works
    Working on prepphase

---
+++ Assets/Scripts/BacktoMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class BacktoMenu : MonoBehaviour {
+public void questMenu() {
+        SceneManager.LoadScene("QuestMenu");
+
+
+	}
+}
--- Assets/Scripts/PrepPhase.cs
+++ Assets/Scripts/PrepPhase.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 using TMPro;
 
@@ -8,18 +9,65 @@ public class PrepPhase : MonoBehaviour {
     public TMP_Text[] IngredientName;
     public TMP_Text[] IngredientPrice;
 
-    
+    public string[] ingredientList = { "Water", "Oil", "Wine" };
+    List<string> displayIngredients;
 
 
     // Use this for initialization
     void Start () {
+        displayIngredients = new List<string>();
+        
+        Ingredients[] ingredients = new Ingredients[3];
+        ingredients[0] = new Ingredients();
+        ingredients[0].ingredientName = "Water";
+        ingredients[0].ingredientPrice = 2;
+        ingredients[0].ingredientQuantity = 1;
+
+        ingredients[1] = new Ingredients();
+        ingredients[1].ingredientName = "Wine";
+        ingredients[1].ingredientPrice = 5;
+        ingredients[1].ingredientQuantity = 1;
+
+        ingredients[2] = new Ingredients();
+        ingredients[2].ingredientName = "Oil";
+        ingredients[2].ingredientPrice = 7;
+        ingredients[2].ingredientQuantity = 1;
 
 
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+        
+
+        for (int count = 0; count < 3; count++)
+        {
+            
+               IngredientName[count].text = ingredients[Random.Range(0, 3)].ingredientName;
+               Debug.Log("test" + IngredientName[count].text);
+               displayIngredients.Add(IngredientName[count].text);
+            if(!displayIngredients.Contains(IngredientName[count].text))
+            {
+        
+            }
+         
+        }
+
+
+        /*
+        for (int count = 0; count <=2; count++)
+        {
+            IngredientPrice[count].text = ingredients[count].ingredientPrice.ToString();
+        }
+        */
+
+
+
+  
+
+
+
+    }
+
+  public void loadBattle()
+    {
+        SceneManager.LoadScene("BattleSystem");
+    }
 }
