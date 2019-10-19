cf052e035d9d725a197d23a41852780b839bcd6a
GregMendoza <mendoza.francisgreg@gmail.com>
2019-10-19 12:09:48 +0800

    Working Invetory
    Inventory elements now add based on the ingredient selected

---
+++ Assets/Scripts/InvManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvManager: MonoBehaviour
+ {
+
+
+	public string[] items = {"Water", "Oil", "Wine", "Herb", "Mushroom","Venom"};
+	public int[] count = {5,5,5,5,5,5};
+     
+    
+	public void buyItems(string name)
+	{
+        for (int x = 0; x < 6; x++)
+		{
+            
+            if (name == items[x])
+            {
+                count[x]++;
+            }
+         
+        }
+	}
+	
+	public void setCount(string name, int amount)
+	{
+		for(int x = 0; x <6; x++)
+		{
+			if(name == items[x])
+			{
+				count[x] = amount;
+			}
+		}
+	}
+	
+}
--- Assets/Scripts/PrepPhase.cs
+++ Assets/Scripts/PrepPhase.cs
@@ -3,33 +3,36 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.EventSystems;
 using TMPro;
 
 public class PrepPhase : MonoBehaviour {
 
     public Player player;
-    public Ingredients dataIngredients;
+    
     public TMP_Text[] IngredientName;
     public TMP_Text[] IngredientPrice;
     public TMP_Text goldValue;
-
-
     public int goldTotal;
+
     public string[] ingredientList = { "Water", "Oil", "Wine" };
     List<string> displayIngredients;
 
+    public Ingredients[] ingredients = new Ingredients[6];
+    public InvManager invmanager;
+
+
 
     // Use this for initialization
-    void Start () {
+    public void Start()
+    {
         goldTotal = player.gold;
         goldValue.text = goldTotal.ToString();
-       
-
         displayIngredients = new List<string>();
-    
+        displayIngredients = Enumerable.ToList(Enumerable.Distinct(displayIngredients));
 
 
-        Ingredients[] ingredients = new Ingredients[3];
+
         ingredients[0] = new Ingredients();
         ingredients[0].ingredientName = "Water";
         ingredients[0].ingredientPrice = 2;
@@ -45,24 +48,54 @@ public class PrepPhase : MonoBehaviour {
         ingredients[2].ingredientPrice = 7;
         ingredients[2].ingredientQuantity = 1;
 
-        
+        ingredients[3] = new Ingredients();
+        ingredients[3].ingredientName = "Herb";
+        ingredients[3].ingredientPrice = 7;
+        ingredients[3].ingredientQuantity = 1;
+
+        ingredients[4] = new Ingredients();
+        ingredients[4].ingredientName = "Mushroom";
+        ingredients[4].ingredientPrice = 7;
+        ingredients[4].ingredientQuantity = 1;
+
+        ingredients[5] = new Ingredients();
+        ingredients[5].ingredientName = "Venom";
+        ingredients[5].ingredientPrice = 7;
+        ingredients[5].ingredientQuantity = 1;
+
+
+
         int count = 0;
         while (count < 3)
         {
             string random = ingredients[Random.Range(0, ingredients.Length)].ingredientName;
-        
             if (!displayIngredients.Contains(random))
             {
-              
                 IngredientName[count].text = random;
                 displayIngredients.Add(IngredientName[count].text);
                 Debug.Log("Item Added " + IngredientName[count].text);
                 count++;
             }
+
         }
-        
+
+        /* for (int priceIndex = 0; priceIndex < ingredients.Length; priceIndex++)
+    {
+        IngredientPrice[priceIndex].text = ingredients[priceIndex].ingredientPrice.ToString();
+    }*/
 
     }
+
+    public void pressBuy() {
+            string whichitem = EventSystem.current.currentSelectedGameObject.name;
+            int detectitem = int.Parse(whichitem);
+
+            invmanager.buyItems(IngredientName[detectitem].text);
+        }
+    
+  
+
+    
 
   public void loadBattle()
     {
